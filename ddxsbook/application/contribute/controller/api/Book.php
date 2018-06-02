e8b0bfe494eca3b7065395bc6d4df4e5c43d711d
yangkaihello <444655291@qq.com>
2018-06-02 12:00:22 +0800

    项目添加

---
+++ ddxsbook/application/contribute/controller/api/Book.php
@@ -0,0 +1,73 @@
+<?php
+namespace app\contribute\controller\api;
+
+use app\contribute\controller\Common;
+use app\contribute\controller\ApiCheck;
+
+use app\contribute\model\Book as ModelBook;
+use app\contribute\model\BookSection;
+use app\contribute\model\Place;
+
+use think\Response;
+use think\Request;
+
+
+class Book extends Common
+{
+
+	use ApiCheck;
+
+	public function _initialize(){
+
+		$this->check((new Place));
+
+	}
+
+	public function get(Request $Request,ModelBook $book){
+
+		$this->bookAuth($Request->get('bookid'));
+
+		$book = $book::get(function ($query) use ($Request){
+			$query->alias('b')
+			->field('b.id,b.title,b.cover,b.description,b.copyright,b.status,b.char_number,c.title as ctitle,u.pen_name as author')
+			->join('category c','b.category_id = c.id','LEFT')
+			->join('user u','b.user_id = u.id','LEFT')
+			->where(['b.id'=>$Request->get('bookid'),'check'=>1]);	
+		});
+
+		//不存在书籍返回无资源
+		if(!$book){
+			return $this->returnXml(['code'=>415,'msg'=>'Not your book'],415);
+		}
+
+		//获取此书所有的章节ID
+		$sectionid = BookSection::where(['book_id'=>$book->id,'check'=>1])->order("sort ASC")->column('id');
+
+		$book->setAttr('cover',imagePath(['table'=>'book','category'=>'cover'],$book->cover));
+		$book->setAttr('copyright',config('book.copyright')[$book->copyright]);
+		$book->setAttr('status',config('book.status')[$book->status]);
+		$book->setAttr('sectionid',$sectionid);
+		
+		return $this->returnXml(
+			['code'=>200,'msg'=>json_decode(json_encode($book),true)],
+			200
+		);
+
+	}
+
+	public function info(Request $Request,ModelBook $book){
+
+		$book = $book::all(function ($query){
+			$query->field('id,title')->whereIn('id',$this->place->posBook)->where(['check'=>1]);
+		});
+		
+		
+		return $this->returnXml(
+			['code'=>200,'msg'=>json_decode(json_encode($book),true)],
+			200
+		);
+
+	}
+
+
+}
---
+++ ddxsbook/application/contribute/model/Mark.php
@@ -0,0 +1,65 @@
+<?php
+namespace app\contribute\model;
+
+use think\Model;
+use app\contribute\model\Book;
+
+class Mark extends Model
+{
+
+
+	public function setting($data)
+	{
+		
+		foreach($data as $key=>$val){
+
+			$arr = ['source_type'=>$key];
+			$save[] = array_merge($arr,$val);
+
+		}
+
+		return $this->saveAll($save);
+	}
+
+	public function homeMark()
+	{
+		$mark = $this->whereExp('source_type','regexp "home_*" ')->select();
+
+		$data = [];
+		foreach($mark as $key=>$val)
+		{
+			$data[$val->source_type] = $val;
+		}
+
+		return $data;
+	}
+
+	public function getAll()
+	{
+		$mark = $this->select();
+
+		$data = [];
+		foreach($mark as $key=>$val)
+		{
+			$data[$val->source_type] = $val;
+		}
+
+		return $data;
+	}
+
+	//获取某项配置
+	public static function getMark($source_type,$column)
+	{
+		$mark = self::where('source_type',$source_type)->column($column);
+		return $mark[0];
+	}
+
+	//推介位的书籍获得
+	public function bookGet($limit)
+	{	
+		return Book::whereIn('id',$this->source_id)->limit($limit)->select();
+	}
+
+
+
+}
