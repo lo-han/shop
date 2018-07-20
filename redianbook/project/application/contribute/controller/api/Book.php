fcf12e1dc1f02e26a6f8bd8821b6e7b6bf92c3ec
yangkaihello <444655291@qq.com>
2018-07-20 11:16:34 +0800

    提交项目

--- redianbook/project/application/contribute/controller/api/Book.php
+++ redianbook/project/application/contribute/controller/api/Book.php
@@ -37,7 +37,7 @@ class Book extends Common
 
 		//不存在书籍返回无资源
 		if(!$book){
-			return $this->returnXml(['code'=>415,'msg'=>'Not your book'],415);
+			return $this->jsonError(['code'=>415,'msg'=>'Not your book'],415);
 		}
 
 		//获取此书所有的章节ID
@@ -64,6 +64,66 @@ class Book extends Common
 		
 		return $this->returnXml(
 			['code'=>200,'msg'=>json_decode(json_encode($book),true)],
+			200
+		);
+
+	}
+
+	//json 处理
+	public function getJson(Request $Request,ModelBook $book){
+
+		$this->bookAuthJson($Request->get('bookid'));
+
+		$book = $book::get(function ($query) use ($Request){
+			$query->alias('b')
+			->field('b.id,b.title,b.cover,b.description,b.copyright,b.status,b.char_number,c.title as ctitle,u.pen_name as author')
+			->join('category c','b.category_id = c.id','LEFT')
+			->join('user u','b.user_id = u.id','LEFT')
+			->where(['b.id'=>$Request->get('bookid')]);	
+		});
+
+		//不存在书籍返回无资源
+		if(!$book){
+			return $this->jsonError(['code'=>415,'msg'=>'Not your book'],415);
+		}
+		
+		$book->setAttr('cover',imagePath(['table'=>'book','category'=>'cover'],$book->cover));
+		$book->setAttr('copyright',config('book.copyright')[$book->copyright]);
+		$book->setAttr('status',$book->status == 1 ? 0 : 1);
+		
+		return $this->jsonSuccess(
+			['code'=>200,'msg'=>json_decode(json_encode($book),true)],
+			200
+		);
+
+	}
+
+	//json 处理
+	public function infoJson(Request $Request,ModelBook $book){
+
+		$book = $book::all(function ($query){
+			$query->field('id,title')->whereIn('id',$this->place->posBook);
+		});
+		
+		
+		return $this->jsonSuccess(
+			['code'=>200,'msg'=>json_decode(json_encode($book),true)],
+			200
+		);
+
+	}
+
+	//json 处理	章节列表
+	public function listJson(Request $Request,ModelBook $book){
+
+		$this->bookAuthJson($Request->get('bookid'));
+
+		//获取此书所有的章节ID
+		$section = BookSection::field('id,CASE WHEN attr = 1 THEN 0 ELSE 1 END as isvip,title')->where(['book_id'=>$Request->get('bookid'),'check'=>1])->order("sort ASC")->select();
+		
+
+		return $this->jsonSuccess(
+			['code'=>200,'msg'=>json_decode(json_encode($section),true)],
 			200
 		);
 
--- redianbook/project/application/contribute/controller/api/Section.php
+++ redianbook/project/application/contribute/controller/api/Section.php
@@ -47,4 +47,33 @@ class Section extends Common
 
 	}
 
+
+	//json 格式返回
+	public function getJson(Request $Request,BookSection $section){
+
+		$section = $section->get(function ($query) use ($Request){
+			$query->field('book_id,title,sort,char,attr as isvip,content')->where('id',$Request->get('chapterid'))->where('check',1);
+		});
+		$book = Book::get(['id'=>$section->book_id]);
+
+		//确认章节的存在
+		if(!$section || !$book){
+			return $this->jsonError(['code'=>415,'msg'=>'Not your chapter'],415);
+		}else{
+			//确认渠道拥有书本权限
+			$this->bookAuthJson($section->book_id);
+		}
+		
+		$section->content = str_replace('</p>', "\n</p>", $section->content);	//格式完善 每一段添加\n
+		$section->content = strip_tags($section->content);	//清除html标签
+		
+		$section->setAttr('isvip',$book->status == 1 ? 0 : 1);
+
+		return $this->jsonSuccess(['code'=>200,'msg'=>json_decode(json_encode($section),true)],200);
+
+
+	}
+
+
+
 }
